eb272d1e15b230ac17b13d51f580688d062702c2
alexscarlatos <alexscarlatos@aol.com>
2018-05-10 21:20:39 -0400

    Cleaning up

--- MakeGraphs.py
+++ MakeGraphs.py
@@ -15,7 +15,7 @@ get_recvd = [10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10, 9, 8, 10, 10, 9, 5, 8, 9
 
 num_nodes = [10, 20, 30, 40, 50, 60]
 stabilization_time = [6.36, 11.52, 16.33, 25.07, 38.46, 51.01]
-stabilization_time_fingers = [,17.126341]
+stabilization_time_fingers = [6.946756, 14.029971, 26.244583, 37.977037, 46.021314, 0]
 
 if __name__ == "__main__":
     '''
@@ -46,15 +46,17 @@ if __name__ == "__main__":
     join_leave_disp = np.log10(join_leave_rate)
     get_success = np.array(get_recvd) / 10
 
-    plt.scatter(fail_rate_disp, insert_hops)
+    plt.scatter(client_rate[10:], get_success[10:])
     plt.title("")
     plt.xlabel("Log Node Failure Rate")
     plt.ylabel("Avg Hops/Req")
     plt.show()
     '''
 
-    plt.scatter(num_nodes, stabilization_time)
-    plt.title("Stabilization Time")
+
+    plt.scatter(num_nodes, stabilization_time_fingers)
+    plt.title("Stabilization Time (w/ Finger Tables)")
     plt.xlabel("Number of Nodes")
     plt.ylabel("Time to Stabilize")
     plt.show()
+
